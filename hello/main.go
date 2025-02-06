8c675ea2f5c0e08a936441f50b0f948efaeaf577
Lawrence Gwam <lawrence@lawrences-mbp.home>
2025-02-06 04:07:20 +0000

    update tests for greeting function

--- hello/main.go
+++ hello/main.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	fmt.Println(greeting())
+}
+
+func greeting() string {
+	return "Hello World!"
 }
--- hello/main_internal_test.go
+++ hello/main_internal_test.go
@@ -1,7 +1,19 @@
 package main
 
+import "testing"
+
 func ExampleMain() {
 	main()
 	// Output:
 	// Hello World!
+}
+
+func TestGreeting(t *testing.T) {
+	expected := "Hello World!"
+	actual := greeting()
+
+	if expected != actual {
+		t.Errorf("Expected string %q does not match %q", expected, actual)
+	}
+
 }
