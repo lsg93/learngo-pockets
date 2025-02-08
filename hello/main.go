5e9889c7d200aebc0cbd2abed937f646fdb7eac5
Lawrence Gwam <lawrence@Lawrences-MacBook-Pro.local>
2025-02-08 02:41:59 +0000

    use map for phrases, add janky tests

--- hello/main.go
+++ hello/main.go
@@ -2,10 +2,44 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println(greeting())
+type language string
+
+/*
+	The example code in the book has the Phrasebook variable lowercased and not exported.
+	The test has basically the exact same map and iterates over it to generate tests
+	Even though this is supposed to be a simple project,
+	Declaring two maps means that both things would need to be updated if new languages were ever added.
+	Opting to export the variable means it can be reused in the test file.
+*/
+
+var Phrasebook = map[language]string{
+	"el": "Χαίρετε Κόσμε",
+	"en": "Hello world",
+	"fr": "Bonjour le monde",
+	"he": "שלום עולם",
+	"ur": "ﻮ ﻠ ﯿ ﮨ",
+	"vi": "Xin chào Thế Giới",
 }
 
-func greeting() string {
-	return "Hello World!"
+// Opted to try and handle errors in functions below even though the book says that comes in later chapters.
+func main() {
+	msg, err := greeting("en")
+	if err != nil {
+		fmt.Printf("Error : %q", err)
+	}
+	fmt.Println(msg)
+}
+
+func greeting(l language) (string, error) {
+	msg, ok := Phrasebook[l]
+	if !ok {
+		var errMsg string
+		if l == "" {
+			errMsg = "no country code provided."
+		} else {
+			errMsg = fmt.Sprintf("country code %q is not supported", l)
+		}
+		return "", fmt.Errorf(errMsg)
+	}
+	return msg, nil
 }
--- hello/main_internal_test.go
+++ hello/main_internal_test.go
@@ -1,19 +1,47 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
-func ExampleMain() {
-	main()
-	// Output:
-	// Hello World!
-}
+// Rather than creating another map just for testing purposes, iterate over the map from main.go.
+// This way tests are always in sync with the data that's actually in use.
 
+// Similarly to the main.go, I've tested for errors (probably badly) even though the book ignores it at this early stage.
 func TestGreeting(t *testing.T) {
-	expected := "Hello World!"
-	actual := greeting()
 
-	if expected != actual {
-		t.Errorf("Expected string %q does not match %q", expected, actual)
-	}
+	t.Run("Works with country codes from phrasebook", func(t *testing.T) {
+		for code, phrase := range Phrasebook {
+			got, _ := greeting(code)
+			want := phrase
+			if got != want {
+				t.Errorf("Expected string %q does not match %q", got, want)
+			}
+
+		}
+	})
+
+	t.Run("Errors as expected when no country code is given", func(t *testing.T) {
+		_, gotErrMsg := greeting("")
+		if gotErrMsg == nil {
+			t.Errorf("Expected error, but did not get one.")
+		}
+		wantErrMsg := "no country code provided."
+		if gotErrMsg.Error() != wantErrMsg {
+			t.Errorf("Expected error mesgosage %q, got %q", wantErrMsg, gotErrMsg)
+		}
+	})
+
+	t.Run("Errors as expected when country code is not found in phrasebook", func(t *testing.T) {
+		_, gotErrMsg := greeting("abc")
+		if gotErrMsg == nil {
+			t.Errorf("country code %q is not supported", "abc")
+		}
+		wantErrMsg := fmt.Sprintf("country code %q is not supported", "abc")
+		if gotErrMsg.Error() != wantErrMsg {
+			t.Errorf("Expected error message %q, got %q", wantErrMsg, gotErrMsg)
+		}
+	})
 
 }
